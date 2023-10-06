068c4ae944eb4b45b8ed4487703a68b1c05fd356
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-10-06 00:39:21 -0300

    first commit in this repository

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # typescript
+
+## Objective
+
+The purpose of creating this repository is to show my learning using TypeScript.
+
+## Initialized
+
+05/20/2023
---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+const input = document.querySelector("input");
+// Calcula o ganho total com base no valor fornecido
+function calculaGanho(value) {
+    const paragraph = document.querySelector("p");
+    if (paragraph) {
+        paragraph.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+    }
+    ;
+}
+// Função chamada quando o valor do input é alterado
+function totalChange() {
+    if (input) {
+        localStorage.setItem("total", input.value);
+        calculaGanho(Number(input.value));
+    }
+}
+const total = localStorage.getItem("total");
+// Inicializa o input com o valor do localStorage, se disponível
+if (input && total) {
+    input.value = total;
+    calculaGanho(Number(input.value));
+}
+// Adiciona um ouvinte de evento para atualizar o localStorage quando o input é alterado
+if (input) {
+    input.addEventListener('keyup', totalChange);
+}
