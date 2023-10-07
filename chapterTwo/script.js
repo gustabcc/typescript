4568d67a64510733c1a6ec2752158d6890225f78
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-10-07 18:07:11 -0300

    use of union type

--- chapterTwo/script.js
+++ chapterTwo/script.js
@@ -1,1 +1,11 @@
 "use strict";
+function toNumber(number) {
+    if (typeof number === 'string') {
+        return Number(number);
+    }
+    if (typeof number === 'number') {
+        return number;
+    }
+    throw new Error("valor deve ser um número ou uma string");
+}
+console.log(toNumber(45));
---
+++ chapterTwo/script.ts
@@ -0,0 +1,11 @@
+function toNumber(number: number | string) {
+    if(typeof number === 'string') {
+        return Number(number);
+    } 
+    if(typeof number === 'number') {
+        return number
+    }
+    throw new Error("valor deve ser um número ou uma string")
+}
+
+console.log(toNumber(45));
