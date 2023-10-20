db5e3ddf55aed811d1e7d4850a185608268a729d
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-10-20 10:21:50 -0300

    union types

---
+++ union_types/index.js
@@ -0,0 +1,26 @@
+"use strict";
+/*
+    union types em TS, serve para definir uma estrutura que pode receber mais de um tipo de dado.
+*/
+function day(dia) {
+    if (typeof dia === 'string') {
+        return Number(dia);
+    }
+}
+let informacao;
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+function toNumber(value) {
+    if (typeof value === 'number') {
+        return value;
+    }
+    else if (typeof value === 'string') {
+        return Number(value);
+    }
+    else {
+        throw new Error('value deve ser um número ou string');
+    }
+}
