f901867bd2532ffeea1fee84de560073e9d0180a
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-10-28 15:06:10 -0300

    classes

---
+++ classe/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// classes são um açucar sintático para funções construtoras
+class Produto {
+    constructor(nome, preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
