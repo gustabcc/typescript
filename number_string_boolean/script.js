0fbe150621f7797d3e4d8eafa6731da95d03ce43
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-10-28 15:37:00 -0300

    teste

---
+++ number_string_boolean/script.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+    string, number e boolean são tipos primitivos do TS.
+    o typeof e utilizado para retornar os valores das variáveis com seus respectivos tipos de dados.
+    o typeof funciona com uma type guard.
+*/
+const frase = 'Front-end';
+const preco = 500;
+const verdade = true;
+if (typeof frase === 'string') {
+    console.log('A frase é uma string');
+}
+// o código acima funciona para os outros tipos também.
